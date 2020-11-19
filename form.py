d766cf9b81a0fb305009b20de5ff6164c9db2c55
nathanielazevedo <nathanielazevedo@hotmail.com>
2020-11-18 20:18:25 -0800

    form

---
+++ form.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField, SelectField, RadioField
+from wtforms.validators import InputRequired, DataRequired, Email, Length
+
+class UserAddForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+    
+class LoginForm(FlaskForm):
+    """Login form."""
+
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
+
+
+class FoodForm(FlaskForm):
+    food_name = StringField('What did you eat?', validators = [InputRequired()])
+    amount = SelectField('How much did you eat?', choices=[('1', 'a little'), ('2', 'some'), ('3', 'a lot')])
+    # feeling = RadioField('choose one', choices=[('1', ''), ('2', ''), ('3', '')])
+
+
+class SearchForm(FlaskForm):
+    food_name = StringField('Search for a food?', validators = [InputRequired()])
+    
