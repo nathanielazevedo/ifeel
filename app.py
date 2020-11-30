5622dcbd0ae0204735b6a2c3b7aa1ed3b1b3a0e5
nathanielazevedo <nathanielazevedo@hotmail.com>
2020-11-29 16:51:03 -0800

    app

--- app.py
+++ app.py
@@ -506,19 +506,29 @@ def display_specific_profile(user_id):
 
 
 
-@app.route('/user', methods=['GET'])
+@app.route('/user', methods=['GET', 'POST'])
 def user():
 
     if not g.user:
         flash("Access unauthorized.", "danger")
         return redirect("/")
 
+
+    form = SearchAddForm()
     foods = (Food
             .query
             .filter(Food.user_id == g.user.id, Food.feeling != 'Null')
             .all())
 
-    return render_template('/user/user.html', foods=foods)
+    if form.validate_on_submit():
+        food = form.search_food_name.data
+        foodlistspot = FoodList.query.filter(FoodList.food_name == food).all()
+        food_id = foodlistspot[0].id
+        foods = (Food.query.filter(Food.user_id == g.user.id, Food.food_id == food_id).all())
+        
+        # return render_template('/user/user.html', foods=foods, form=form)
+
+    return render_template('/user/user.html', foods=foods, form=form)
     
 
 
@@ -655,7 +665,16 @@ def graph(food_id):
         return redirect('/login')
 
     alldata = Food.query.filter(Food.food_id == food_id).all()
-    foodname = alldata[0].food_name
+    try:
+        foodname = alldata[0].food_name
+    except:
+
+        form = SearchForm()
+        allfoods = FoodList.query.all()
+
+
+        flash("Sorry, we don't have any data on that food yet")
+        return render_template('/search/search-food.html', form=form, allfoods=allfoods)
     foodsymptomslists = []
     foodsymptoms = []
     fooddata = []
@@ -696,8 +715,16 @@ def graph(food_id):
 
 
     graph2 = requests.get(f"https://quickchart.io/chart?c={{type:'bar',data:{{labels:{symptoms},datasets:[{{label:'Number of reports per symptom after eating {foodname}',data:{symptomslists}}}]}}}}")
+
     
-    return render_template('graph.html', graph=graph, graph2=graph2, foodname=foodname)
+    nutritional_info = requests.get(f"https://api.spoonacular.com/food/ingredients/{alldata[0].info.spoonacular_id}/information?amount=1&apiKey=b7e7c1efd70843b7a897ec8eb3717e34&unit=serving").json()
+    # pdb.set_trace()
+    # try:
+    #     nutritional_info['nutrition']['nutrients'][11]
+    # except:
+    #     nutritional_info = 'none'
+    nutrition = cleanNutrition(nutritional_info)
+    return render_template('graph.html', graph=graph, graph2=graph2, foodname=foodname, nutrition=nutrition)
 
 
 
@@ -791,3 +818,145 @@ def addspoontodatabase(food):
 
 
 
+def cleanNutrition(nutritional_info):
+    nutrition = {}
+    try:
+        calories = nutritional_info['nutrition']['nutrients'][0]
+        nutrition['calories'] = calories
+    except:
+        nutrition['calories'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+    try:
+        fat = nutritional_info['nutrition']['nutrients'][1]
+        nutrition['fat'] = fat
+    except:
+        nutrition['fat'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        saturated_fat = nutritional_info['nutrition']['nutrients'][2]
+        nutrition['saturated_fat'] = saturated_fat
+    except:
+        nutrition['saturated_fat'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        carbohydrates = nutritional_info['nutrition']['nutrients'][3]
+        nutrition['carbohydrates'] = carbohydrates
+    except:
+        nutrition['carbohydrates'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        sugar = nutritional_info['nutrition']['nutrients'][5]
+        nutrition['sugar'] = sugar
+    except:
+        nutrition['sugar'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        cholesterol = nutritional_info['nutrition']['nutrients'][6]
+        nutrition['cholesterol'] = cholesterol
+    except:
+        nutrition['cholesterol'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+    try:   
+        sodium = nutritional_info['nutrition']['nutrients'][7]
+        nutrition['sodium'] = sodium
+    except:
+        nutrition['sodium'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+    try:
+        protein = nutritional_info['nutrition']['nutrients'][8]
+        nutrition['protein'] = protein
+    except:
+        nutrition['protein'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+    try:       
+        vitaminC = nutritional_info['nutrition']['nutrients'][9]
+        nutrition['vitaminC'] = vitaminC
+    except:
+        nutrition['vitaminC'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+    try:   
+        fiber = nutritional_info['nutrition']['nutrients'][11]
+        nutrition['fiber'] = fiber
+    except:
+        nutrition['fiber'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        iron = nutritional_info['nutrition']['nutrients'][21]
+        nutrition['iron'] = iron
+    except:
+        nutrition['iron'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        calcium = nutritional_info['nutrition']['nutrients'][22]
+        nutrition['calcium'] = calcium
+    except:
+        nutrition['calcium'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }
+
+    try:
+        vitaminA = nutritional_info['nutrition']['nutrients'][23]
+        nutrition['vitaminA'] = vitaminA
+    
+    except:
+        nutrition['vitaminA'] =  {
+                "title": "-",
+                "amount": "-",
+                "unit": "-",
+                "percentOfDailyNeeds": '-'
+            }   
+    
+    return nutrition
